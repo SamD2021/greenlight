f812421a977c5028dd2548e2ef2ba00d12587d8c
SamD2021 <samuelramos852@gmail.com>
2025-04-28 02:37:32 -0400

    greenlight-cli/src/main.rs: Implement Logging to print a start message and total check for debugging
    Since this tool is doing some important tasks, we should have robust
    logging functionality for it.
    We will use the tracing crate to accomplish this, since it gives us a
    pretty lightweight solution to our logging needs.
    It also supports changing log level by setting the `RUST_LOG` env to the
    appropriate logging level needed without changing code or config for
    quick interactions (e.g RUST_LOG=debug greenlight)

--- greenlight-cli/src/main.rs
+++ greenlight-cli/src/main.rs
@@ -1,9 +1,13 @@
 mod cli;
 use clap::Parser;
 use cli::Args;
-use greenlight_lib::{config::Config, errors::GreenlightError};
+use greenlight_lib::{checks::Check, config::Config, errors::GreenlightError};
 use std::{collections::HashSet, path::PathBuf, process::ExitCode};
+use tracing::{debug, info};
+use tracing_subscriber::fmt;
 fn main() -> Result<ExitCode, GreenlightError> {
+    fmt::init(); // Setup logging once!
+    info!("Starting Greenlight!");
     let args = Args::parse();
     let config_path = match args.config_path {
         Some(config_path) => config_path,
@@ -11,15 +15,28 @@ fn main() -> Result<ExitCode, GreenlightError> {
     };
     let config: Config = Config::from_path(&config_path)?;
 
-    let included_checks = args
+    let included_checks: HashSet<Check> = args
         .include_checks
         .or_else(|| config.checks.include.clone()) // clone because Option<Vec> needs to be moved
-        .unwrap_or_default();
-
-    let excluded_checks = args
+        .unwrap_or_default()
+        .into_iter()
+        .collect();
+    let excluded_checks: HashSet<Check> = args
         .exclude_checks
         .or_else(|| config.checks.exclude.clone())
+        .unwrap_or_default()
+        .into_iter()
+        .collect();
+    let target = args
+        .target
+        .or_else(|| Some(config.system.target().clone()))
         .unwrap_or_default();
+    let total_checks: HashSet<_> = included_checks
+        .union(&target.default_checks())
+        .filter(|c| !excluded_checks.contains(c))
+        .cloned()
+        .collect();
+    debug!("Total checks ({}): {:?} ", total_checks.len(), total_checks);
 
     Ok(ExitCode::SUCCESS)
 }
